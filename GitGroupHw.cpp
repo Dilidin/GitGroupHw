b013c1d0b5cf14b1c5265a893bc0eeeb85d8e9ce
Diana <adoevcevadi@gmail.com>
2024-11-09 16:13:15 +0200

    Add project files.

---
+++ GitGroupHw.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    std::cout << "Enter first number: \n";
+    std::cin >> a;
+    std::cout << "Enter second number: \n";
+    std::cin >> b;
+}
